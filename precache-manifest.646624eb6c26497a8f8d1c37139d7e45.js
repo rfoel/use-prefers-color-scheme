88ddd28f5998cf88eba612848d197c08bf019a56
Rafael <rafaelfr@outlook.com>
2020-05-11 13:52:48 -0300

    Updates

---
+++ precache-manifest.646624eb6c26497a8f8d1c37139d7e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c176b2e9aafe2d02d01c506dbafc7a49",
+    "url": "/use-prefers-color-scheme/index.html"
+  },
+  {
+    "revision": "30f64473438bd4478cd6",
+    "url": "/use-prefers-color-scheme/static/css/main.41fadb89.chunk.css"
+  },
+  {
+    "revision": "426fc73ed73ac9c00ce3",
+    "url": "/use-prefers-color-scheme/static/js/2.ee5964dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/use-prefers-color-scheme/static/js/2.ee5964dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f64473438bd4478cd6",
+    "url": "/use-prefers-color-scheme/static/js/main.3fdf97e0.chunk.js"
+  },
+  {
+    "revision": "8ebc342631b7300f2bb8",
+    "url": "/use-prefers-color-scheme/static/js/runtime-main.fc9d4e2f.js"
+  }
+]);
